f2155e002960b5039fc92845df3f2aa7268eb426
木伏 徹 <s1170230@gmail.com>
2022-02-03 20:40:24 +0900

    quote done

---
+++ Quotes/script.js
@@ -0,0 +1,25 @@
+const simpleQuote = [
+    {
+        name:"quote author1",
+        quote:"quote no1"
+    },    {
+        name:"quote author2",
+        quote:"quote no2"
+    },    {
+        name:"quote author3",
+        quote:"quote no3"
+    }
+]
+
+const quoteBtn = document.querySelector("#quoteBtn");
+const quoteAuthor = document.querySelector("#quoteAuthor");
+const quote = document.querySelector("#quote");
+
+quoteBtn.addEventListener("click",displayQuote);
+
+function displayQuote(){
+    let number = Math.floor(Math.random()*simpleQuote.length);
+    // console.log(simpleQuote[number].name);
+    quoteAuthor.innerHTML = simpleQuote[number].name;
+    quote.innerHTML = simpleQuote[number].quote;
+}
